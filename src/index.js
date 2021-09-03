3a09ca27046d15141b1646fe2ce08a3058b11116
Kelvyn dos Santos <kelvynsantos10@outlook.com>
2021-09-02 23:46:45 -0300

    confirmar

--- src/index.js
+++ src/index.js
@@ -1,24 +1,32 @@
             import './index.css' 
             import React from 'react' /* para se utilizar o JSX*/
             import ReactDOM from 'react-dom' /*react-dom utiliza-se apenas dentro deste arquivo */
-            // import Primeiro from './components/Primeiro' 
+            import Primeiro from './components/Primeiro' 
             // import ComParametro from './components/ComParametro' 
-            import compoFilhos from './components/compoFilhos'
+            // import compoFilhos from './components/compoFilhos' 
+            import Card from './components/layout/Card'
             
             ReactDOM.render( 
-                <div> 
+                <div>  
+                    <Card titulo="Primeiro componente"> 
+                    <Primeiro />
+                    </Card> 
+                    <Card titulo="Exercício X"> 
+                        conteudo
+                    </Card>
                 {/*<h1>Olá React</h1>, JSX
                 <Primeiro />, 
                 <ComParametro titulo="Esse é o título" subtitulo="Esse é o subtítulo"/> */} 
-                <compoFilhos> 
-                    <ul> 
-                        <li>Ana<li>
-                        <li>Bia</li> 
-                        </li>Daniel</li>
-                        <li>Carlos</li> 
-                    </ul>                
-                </compoFilhos>
+                    {/* <compoFilhos> 
+                        <ul> 
+                            <ul> 
+                                 <li>Ana</li>
+                                 <li>Bia</li> 
+                                 <li>Carlos</li> 
+                                 <li>Daniel</li>
+                            </ul>        
+                        </ul>
+                    </compoFilhos> */}
                 </div>,
                 document.getElementById('root')  
-                /*onde quero renderizar,div root*/
             )
